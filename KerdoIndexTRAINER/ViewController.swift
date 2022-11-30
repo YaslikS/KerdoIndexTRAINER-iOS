8e7d26c896b425f972c58d3e0da519605bc40d84
YaslikS <perejaslov9200@gmail.com>
2022-11-30 18:40:05 +0300

    Initial Commit

---
+++ KerdoIndexTRAINER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KerdoIndexTRAINER
+//
+//  Created by Вячеслав Переяслов on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
